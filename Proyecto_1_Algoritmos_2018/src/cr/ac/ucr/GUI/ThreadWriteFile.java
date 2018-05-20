be2f0f608e25ef39803f343c5aff7cdf041a02f4
melvinastorga <melvin.astorga@gmail.com>
2018-05-19 21:58:34 -0600

    Ya guarda archivos de clientes.txt con el Hilo

--- Proyecto_1_Algoritmos_2018/src/cr/ac/ucr/GUI/ThreadWriteFile.java
+++ Proyecto_1_Algoritmos_2018/src/cr/ac/ucr/GUI/ThreadWriteFile.java
@@ -3,12 +3,15 @@ package cr.ac.ucr.GUI;
 
 import cr.ac.ucr.Domain.Client;
 import cr.ac.ucr.Files.ClientFile;
+import cr.ac.ucr.Files.Write_Read_Files;
+import java.io.PrintStream;
 import java.util.LinkedList;
 
 
 public class ThreadWriteFile extends Thread {
     
        ClientFile clietfile;
+       Write_Read_Files write;
     
     Insert_Client insertClient = new Insert_Client();
     
@@ -17,7 +20,9 @@ public class ThreadWriteFile extends Thread {
     public void run() {
         
    //Client client=new Client(tft_ID.getText(), tft_Name.getText(),tft_LastName1.getText(),tft_LastName2.getText(),tft_Email.getText(), tft_Phone.getText(),tft_Province.getText(),tft_Address.getText());
-   clietfile =new ClientFile();
+   //clietfile =new ClientFile();
+   write = new Write_Read_Files();
+        PrintStream ps = write.getPrintStream("clients.txt");
    int i = 0;
    
    try{
@@ -27,9 +32,10 @@ public class ThreadWriteFile extends Thread {
    while(!fileLinkedListClient.isEmpty()){
        
       Client client = fileLinkedListClient.get(i);
+      ps.println(client.getID()+"   "+client.getName()+"   "+client.getLastName1()+"   "+client.getLastName2()+"   "+client.getEmail()+"   "+client.getPhone()+"   "+client.getProvince()+"   "+client.getAddress());
        i++;
        
-       clietfile.writeClients(client,true);
+       //clietfile.writeClients(client,true);
    }
    
    }catch (InterruptedException ex) {
